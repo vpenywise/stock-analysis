44d9d698019354d9bef23e5e1aaad508ac9610fb
vpenywise <68743149+vpenywise@users.noreply.github.com>
2021-12-08 23:50:07 +0200

    Add files via upload

---
+++ StockFileApplication.java
@@ -0,0 +1,47 @@
+package client;
+
+import java.io.IOException;
+import java.util.*;
+
+import fileprocessors.StockFileData;
+import fileprocessors.StockFileReader;
+
+public class StockFileApplication {
+
+	public static void main(String args[]) throws IOException{
+		StockFileReader fr = new StockFileReader("table.csv");
+		
+		List<HashMap<String, Double>> dataResult = populateStockFileData(fr.getHeaders(), fr.readFileData());
+		StockFileData fileData = new StockFileData();
+		fileData.addData(dataResult);
+		fileData.printData();
+		System.out.println(dataResult.size());
+
+	}
+	/**
+	 * Complete the method body so that it returns the given structure needed to 
+	 * populate the data field in the StockFileData class. 
+	 * @param headers
+	 * @param lines
+	 * @return List
+	 */
+	public static List<HashMap<String, Double>> populateStockFileData(List<String> headers, List<String> lines){
+		List<HashMap<String, Double>> dataResult = new ArrayList<>();
+
+		for (String line : lines) {
+			String[] values = line.split(",");
+			int count = 0;
+			HashMap<String, Double> headerValueMap = new HashMap<>();
+			for (String value : values) {
+				double dbl = Double.parseDouble(value);
+				headerValueMap.put(headers.get(count), dbl);
+				count++;
+			}
+			dataResult.add(headerValueMap);
+		}
+
+		return dataResult;
+	}
+	
+	
+}
